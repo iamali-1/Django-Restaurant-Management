417e09fc19a6ec73cdbde648e24a1e0cb1c8fb35
iamali-1 <syedmuhammadalihaider01@gmail.com>
2026-03-25 17:09:01 +0500

    added data to the database

--- ORM/scripts/orm_scripts.py
+++ ORM/scripts/orm_scripts.py
@@ -1,12 +1,16 @@
-from ORM.models import Restaurant, Rating, Sale, Staff
+from ORM.models import Restaurant, Rating, Sale, Staff, StaffRestaurant
 from django.utils import timezone
 from django.db import connection
 from django.contrib.auth.models import User
 from pprint import pprint
 from django.db.models import Prefetch
+import random
 
 
 def run():
 
-    restaurant = Restaurant.objects.get(pk = 21)
-    print(restaurant.staff_set.all())
+    staff, created = Staff.objects.get_or_create(name="John Wick")
+    staff.restaurant.set(
+        Restaurant.objects.all()[:10],
+        through_defaults={"salary": random.randint(20_000, 80_000)},
+    )
